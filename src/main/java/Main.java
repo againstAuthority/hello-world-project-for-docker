eac57e56125594f8150c344fb1b6fc3cb87cd7bb
againstAuthority <krotenko94@mail.ru>
2019-05-06 23:01:31 +0300

    Commit with new pom for deploying ear to wildfly via jenkins

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Thread mainThread = Thread.currentThread();
+        System.out.println("Текущий поток: " + mainThread.getName());
+
+// Меняем имя и выводим в текстовом поле
+        mainThread.setName("HelloProjectThread");
+        System.out.println("Новое имя потока: " + mainThread.getName());
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter some number: ");
+        int input = in.nextInt();
     }
 }
